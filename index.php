7134547013b6b3b660234c76d65535b018e609d7
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-08 11:01:07 +0200

    adding css

--- index.php
+++ index.php
@@ -4,49 +4,83 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- <link rel="stylesheet" href="./assets/css/style.css"> -->
+    <link rel="stylesheet" href="./assets/sass/style.css">
     <title>AZ_Store</title>
 </head>
+
+
+<!-- nav -->
 <header>
+    <nav>
     <h1>AZ[store]</h1>
     <a href="empty">Home</a>
     <a href="empty">About</a>
     <a href="empty">Products</a>
     <a href="empty">Contact</a>
     <a href="empty">Login</a>
-    <img src="./assets/img/shopping-cart.svg" alt="shopping-cart">
+    <img id="shopping_cart_logo" src="./assets/img/shopping-cart.svg" alt="shopping-cart">
+    </nav>
 </header>
 <body>
+
+
+
+<!-- store section -->
 <section class="store">
-<h1 class="slogan_1">Shoe the right one.</h1>
-<button id="Move_to_store">See out store</button>
-<img src="./assets/img/shoe_one.png" alt="shoe_one">
+<div>
+    <h1 class="slogan_1">Shoe the right one.</h1>
+    <button id="Move_to_store">See our store</button>
+</div>
+<div>
+<img  id="big_shoe_1"src="./assets/img/shoe_one.png" alt="shoe_one">
 <p id="nike">NIKE</p>
+</div>
 </section>
+
+
+
+
+<!-- products section -->
 <section class="products">
-    <h2 id ="products">Our last products</h2>
-    <div id="div_shoe_1"><img src="./assets/img/shoe_one.png" alt="shoe_one">
-<h3>NIKE Air</h3>
-<p>234€</p>
-<button class="add_to_cart">add to cart</button></div>
-       <div id="div_shoe_2"><img src="./assets/img/shoe_one.png" alt="shoe_one">
-<h3>NIKE Air</h3>
-<p>234€</p>
-<button class="add_to_cart">add to cart</button></div>
-    <div id="div_shoe_3"><img src="./assets/img/shoe_one.png" alt="shoe_one">
-<h3>NIKE Air</h3>
-<p>234€</p>
-<button class="add_to_cart">add to cart</button></div>
-    <div id="div_shoe_4"><img src="./assets/img/shoe_one.png" alt="shoe_one">
-<h3>NIKE Air</h3>
-<p>234€</p>
-<button class="add_to_cart">add to cart</button></div>
+<h2 id ="products">Our last products</h2>
+<div class="products_shoes" id="div_shoe_1">
+    <img src="./assets/img/shoe_one.png" alt="shoe_one">
+    <h3>NIKE Air</h3>
+    <p>234€</p>
+    <button class="add_to_cart">add to cart</button>
+</div>
+<div class="products_shoes" id="div_shoe_2">
+    <img src="./assets/img/shoe_one.png" alt="shoe_one">
+    <h3>NIKE Air</h3>
+    <p>234€</p>
+    <button class="add_to_cart">add to cart</button>
+</div>
+<div class="products_shoes" id="div_shoe_3">
+    <img src="./assets/img/shoe_one.png" alt="shoe_one">
+    <h3>NIKE Air</h3>
+    <p>234€</p>
+    <button class="add_to_cart">add to cart</button>
+</div>
+<div class="products_shoes" id="div_shoe_4">
+    <img src="./assets/img/shoe_one.png" alt="shoe_one">
+    <h3>NIKE Air</h3>
+    <p>234€</p>
+    <button class="add_to_cart">add to cart</button>
+</div>
 </section>
+
+
+<!-- citations section -->
 <section class="citation">
     <img src="./assets/img/shoe_two.png" alt="shoe_two">
     <h1 id="slogan_2">We provide you the best quality.</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptas minima illo repudiandae aspernatur veniam praesentium eius harum distinctio earum iste repellat deserunt, quae, explicabo rerum sequi. Suscipit, odit blanditiis?</p>
 </section>
+
+
+
+<!-- commentaires section -->
 <section class="commentaires">
 <div id="emily">
     <img src="./assets/img/image-emily.jpg" alt="emily">
@@ -66,10 +100,13 @@
 </section>
 
 
+
+<!-- footer contacts -->
 </body>
-
-
-
-
-<footer></footer>
+<footer>
+    <a href="empty">Home</a>
+    <a href="empty">About</a>
+    <a href="empty">Products</a>
+    <a href="empty">Contact</a>
+</footer>
 </html>
