fd1ab3ad15aa044914b2f2c2e8ab8edba37c3071
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-08 10:39:32 +0200

    adding store/products/slogan/commentaires

--- index.php
+++ index.php
@@ -16,13 +16,54 @@
     <a href="empty">Login</a>
     <img src="./assets/img/shopping-cart.svg" alt="shopping-cart">
 </header>
-
-
-
-
 <body>
-
-
+<section class="store">
+<h1 class="slogan_1">Shoe the right one.</h1>
+<button id="Move_to_store">See out store</button>
+<img src="./assets/img/shoe_one.png" alt="shoe_one">
+<p id="nike">NIKE</p>
+</section>
+<section class="products">
+    <h2 id ="products">Our last products</h2>
+    <div id="div_shoe_1"><img src="./assets/img/shoe_one.png" alt="shoe_one">
+<h3>NIKE Air</h3>
+<p>234€</p>
+<button class="add_to_cart">add to cart</button></div>
+       <div id="div_shoe_2"><img src="./assets/img/shoe_one.png" alt="shoe_one">
+<h3>NIKE Air</h3>
+<p>234€</p>
+<button class="add_to_cart">add to cart</button></div>
+    <div id="div_shoe_3"><img src="./assets/img/shoe_one.png" alt="shoe_one">
+<h3>NIKE Air</h3>
+<p>234€</p>
+<button class="add_to_cart">add to cart</button></div>
+    <div id="div_shoe_4"><img src="./assets/img/shoe_one.png" alt="shoe_one">
+<h3>NIKE Air</h3>
+<p>234€</p>
+<button class="add_to_cart">add to cart</button></div>
+</section>
+<section class="citation">
+    <img src="./assets/img/shoe_two.png" alt="shoe_two">
+    <h1 id="slogan_2">We provide you the best quality.</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptas minima illo repudiandae aspernatur veniam praesentium eius harum distinctio earum iste repellat deserunt, quae, explicabo rerum sequi. Suscipit, odit blanditiis?</p>
+</section>
+<section class="commentaires">
+<div id="emily">
+    <img src="./assets/img/image-emily.jpg" alt="emily">
+    <h3>Emily from XYZ</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eum exercitationem quas iste doloremque vitae mollitia sint quibusdam placeat deserunt? Ex recusandae provident optio cupiditate tempora vel totam delectus eius!</p>
+</div>
+<div id="Thomas">
+    <img src="./assets/img/image-thomas.jpg" alt="emily">
+    <h3>Thomas from corporate</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eum exercitationem quas iste doloremque vitae mollitia sint quibusdam placeat deserunt? Ex recusandae provident optio cupiditate tempora vel totam delectus eius!</p>
+</div>
+<div id="jennie">
+    <img src="./assets/img/image-jennie.jpg" alt="emily">
+    <h3>Jennie from Nike</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eum exercitationem quas iste doloremque vitae mollitia sint quibusdam placeat deserunt? Ex recusandae provident optio cupiditate tempora vel totam delectus eius!</p>
+</div>
+</section>
 
 
 </body>
