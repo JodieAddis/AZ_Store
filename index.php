fc91b3ca55e5f3d682894bc7a9a919dc0f9dd169
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-08 10:05:58 +0200

    adding header

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AZ_Store</title>
 </head>
+<header>
+    <h1>AZ[store]</h1>
+    <a href="empty">Home</a>
+    <a href="empty">About</a>
+    <a href="empty">Products</a>
+    <a href="empty">Contact</a>
+    <a href="empty">Login</a>
+    <img src="./assets/img/shopping-cart.svg" alt="shopping-cart">
+</header>
+
+
+
+
 <body>
-    
+
+
+
+
 </body>
+
+
+
+
+<footer></footer>
 </html>
