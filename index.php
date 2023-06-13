7ccdec5478784d4250d071524f8570f633915642
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-06-13 19:13:35 +0200

    adding css to product section

--- index.php
+++ index.php
@@ -90,40 +90,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 <!-- store section -->
-<section class="store">
-<div>
-    <h1 class="slogan_1 text-white text-5xl">Shoe the right <b class="text-[#2563eb]" >one</b>.</h1>
+<section class="store flex-row items-center justify-center border-b-2 border-b-solid border-b-white">
+<div class ="w-1/2 h-20 flex items-center justify-center">
+    <h1 class="slogan_1 text-white text-7xl">Shoe the right <b class="text-[#2563eb]" >one</b>.</h1>
     <button id="Move_to_store" class="text-white p-4 m-3 bg-gradient-to-l from-[#203e91] to-[#397def]">See our store</button>
 </div>
-<div>
-<img  id="big_shoe_1"src="./assets/img/shoe_one.png" alt="shoe_one" >
-<p id="nike" class="text-white">NIKE</p>
+<div class="w-1/2 flex items-center justify-items-start">
+<img  id="big_shoe_1"src="./assets/img/shoe_one.png" alt="shoe_one" class="m-20" >
+<!-- <p id="nike" class="text-white absolute text-9xl">NIKE</p> -->
 </div>
 </section>
 
 <!-- products section -->
-<section class="products">
+<section class="products inline-block w-full">
+    <div class="m-20">
 <h2 id ="products"class ="text-white text-5xl"><b class="text-[#2563eb]">Our</b> last products</h2>
+</div>
+<div class="flex items-center justify-around">
 <?php
 $i=0;
             foreach ($items as $item) {
-                echo '<div class="products_shoes " id=' . $item["id"] . '>
+                echo '<div class="products_shoes flex flex-col items-center h-80 w-80 m-l-20" id=' . $item["id"] . '>
                     <img class="text-white" src=' . $item['image_url'] . '> 
-                    <h3 class="text-white text-emphasized">' . $item['product'] . '</h3> 
-                    <p class="text-white">' . $item['price'] . '</p>
+                    <div class="flex w-full justify-between m-5">
+                    <div class="p-5">
+                    <h3 class="text-white text-emphasized font-bold text-xl">' . $item['product'] . '</h3> 
+                    <p class="text-white text-xl">' . $item['price'] . '</p>
+                    </div>
+                    <div class=" p-5">
                     <form method="post" action="">
-                        <button name="button' . $i . '" class="add_to_cart text-white bg-gradient-to-l from-[#203e91] to-[#397def] p-2 " type="submit">add to cart</button>
+                        <button name="button' . $i . '" class="add_to_cart text-white bg-gradient-to-l from-[#203e91] to-[#397def] p-2 text-xl" type="submit">add to cart</button>
+                        </div>
+                        </div>
                     </form>
                 </div>';
                 $i++;
             }
 
 ?>
+</div>
 </section>
 
 
 <!-- citations section -->
-<section class="citation">
+<section class="citation m-20">
     <img src="./assets/img/shoe_two.png" alt="shoe_two" >
     <h1 id="slogan_2" class="text-white text-5xl">We provide you the <b class="text-[#2563eb]">best</b> quality.</h1>
     <p class="text-white text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptas minima illo repudiandae aspernatur veniam praesentium eius harum distinctio earum iste repellat deserunt, quae, explicabo rerum sequi. Suscipit, odit blanditiis?</p>
